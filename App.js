6631e445d0550d3d9f8751f1b26b3422bef4fe6f
Igor Yovka <igor.yovka@gmail.com>
2018-05-26 14:09:55 +0300

    init commit

---
+++ App.js
@@ -0,0 +1,115 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    Image,
+    FlatList
+} from 'react-native';
+
+const proxy = process.env['proxy'];
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchFor: '',
+            images: [],
+            results: true
+        };
+    }
+
+    handleSearchQuery = () => {
+        const {images, searchFor} = this.state;
+        fetch(`${proxy}/images/${searchFor}`)
+            .then((res) => res.json())
+            .then((result) => {
+                this.setState({
+                    images: result.data,
+                    results: true
+                })
+            })
+    };
+
+    handleTextInput = (searchFor) => {
+        this.setState({searchFor})
+    };
+
+    render() {
+        const {images, searchFor} = this.state;
+
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Text style={styles.textAtSearch}>Input request:</Text>
+                    <TextInput
+                        placeholder="Search"
+                        style={styles.searchInputStyles}
+                        onChangeText={this.handleTextInput}
+                        onSubmitEditing={this.handleSearchQuery}
+                        underlineColorAndroid={'rgba(0,0,0,0)'}
+                        placeholderTextColor={'#322d4c'}
+                        value={searchFor}
+                    />
+                </View>
+                <FlatList
+                    data={images}
+                    numColumns={2}
+                    style={styles.listStyle}
+                    renderItem={({item, index}) =>
+                        <Image style={styles.imageSize}
+                               source={{
+                                   uri: item.src
+                               }}/>
+                    }
+                    keyExtractor={(item, index) => String(index)}
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 20,
+        backgroundColor: '#bcd9dc',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column'
+    },
+    text: {
+        alignSelf: 'center',
+        color: '#000',
+        fontWeight: 'bold'
+    },
+    textAtSearch: {
+        alignSelf: 'center',
+        color: '#000',
+        fontWeight: 'bold',
+        marginTop: 10,
+        marginBottom: 10
+    },
+    searchInputStyles: {
+        height: 40,
+        borderColor: '#FFF',
+        borderWidth: 1,
+        minWidth: 200,
+        color: '#02004c',
+        fontWeight: 'bold',
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    imageSize: {
+        width: 100,
+        height: 100,
+        margin: 10
+    },
+    listStyle: {
+        flex: 1,
+        marginTop: 10,
+        flexDirection: 'column',
+        flexWrap: 'wrap'
+    }
+});
---
+++ server.js
@@ -0,0 +1,47 @@
+/**
+ * Created by Jordan3D on 5/24/2018.
+ */
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http').Server(app);
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8080,
+    ip = process.env.IP || '0.0.0.0';
+
+const https = require('https');
+const parseHtml = require('node-html-parser').parse;
+
+app.use(express.static('./'));
+app.get('/images/:query', function(req,res){
+    var query = req.params.query;
+    console.log(encodeURI(query));
+
+    https.get(`https://www.google.com.ua/search?q=${encodeURI(query)}&source=lnms&tbm=isch&sa=X&ved=0ahUKEwi09djhmKPbAhWRJVAKHdufDKUQ_AUICygC&biw=1280&bih=893`, (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            const parsedHTML = parseHtml(data);
+
+            const images = parsedHTML.querySelectorAll('img');
+
+            const imagesSrc = images.map((i)=> ({src: i.rawAttributes.src})).slice(1);
+
+            res.send(JSON.stringify({data: imagesSrc}));
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+});
+
+http.listen(port, ip, function () {
+    console.log(`App is listening on *:${port}`);
+    // console.log(matchShort.getLatestMatch());
+});
